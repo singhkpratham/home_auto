0d2120b61f083d608120b8bdd539362ddbcbe968
kumar.singh <kumar.pratham@learner.manipal.edu>
2017-10-27 21:06:12 +0530

    html page ready by taking html css codes from internet, single slider checkbox using bootstrap etc

--- page1/urls.py
+++ page1/urls.py
@@ -13,10 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
-
+from django.conf.urls import url
+from . import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'page1/', include("page1.urls"))
+    url(r'^$',views.index)
 ]
--- page1/views.py
+++ page1/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def index(request):
+    print(request.POST)
+    return render(request, 'page1.html', {'request':request})
