1a9823aeb68286431e3e5195d6c0523d232cc832
singhkpratham <singhkpratham@gmail.com>
2017-10-28 13:21:46 +0530

    pin activation trial

--- page1/views.py
+++ page1/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render,HttpResponse
-
+import RPi.GPIO as GPIO
+import time
 # Create your views here.
 
 def index(request):
     print(request.POST)
+    if request.POST['switch']:
+        pin4(request.POST['switch'])
     return render(request, 'page1.html', {'request':request})
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+
+def pin4(mode=False):
+    while mode:
+        GPIO.output(4, True)
+
+
+##GPIO.output(4, False)
+
+##GPIO.cleanup()
+
+##GPIO.setmode(GPIO.BCM)
+##GPIO.setup(4,GPIO.IN)
+
+##for i in range(10000):
+##    time.sleep(0.05)
+##    print(GPIO.input(4))
+
+GPIO.cleanup()
