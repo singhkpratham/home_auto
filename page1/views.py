25efafcb3454f097552c3fbb52122e8eb9c3c107
singhkpratham <singhkpratham@gmail.com>
2017-10-29 12:57:26 +0530

    added TODO list in views.py of page1

--- page1/views.py
+++ page1/views.py
@@ -17,3 +17,7 @@ def index(request):
     pinStatus = {p:'checked' if GPIO.input(p) == 1 else '' for p in pins }
     print(pinStatus)
     return render(request, 'page1.html', {'request':request,'pins':pinStatus})
+
+
+# TODO: 1. add specific time and timer to start the pins. this will most probably require threading
+# TODO: 2. login page, only registered users can log in and turn pins on/off
